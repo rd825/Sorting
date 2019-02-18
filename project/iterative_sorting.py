ddbd68c71c91a6f0c0818495f20058ff06c4431f
Rahul Desai <rd825@georgetown.edu>
2019-02-18 15:16:26 -0500

    Did bubble sort

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -36,7 +36,10 @@ print(insertion_sort(arr))
 
 
 def bubble_sort(arr):
-
+    for i in range(0, len(arr) - 1):
+        for j in range(0, len(arr) - 1 - i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
 

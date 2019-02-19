96660c8ad6e84fe4b5edc7f79fb22c926f4f3a41
Rahul Desai <rd825@georgetown.edu>
2019-02-19 15:47:23 -0500

    Completed binary sort iterative

--- project/searching.py
+++ project/searching.py
@@ -17,19 +17,29 @@ print(linear_search(arr, 22))
 
 
 def binary_search(arr, target):
-
     if len(arr) == 0:
         return -1  # array empty
 
     low = 0
     high = len(arr)-1
 
-    # TO-DO: add missing code
+    while low <= high:
+        mid = (low+high)//2
+        if target < arr[mid]:
+            high = mid-1  # eliminate RHS
+        elif target > arr[mid]:
+            low = mid+1  # eliminate LHS
+        else:
+            return mid
 
     return -1  # not found
 
 
+print(binary_search([1, 2, 3, 4, 7], 81))
+
 # STRETCH: write a recursive implementation of Binary Search
+
+
 def binary_search_recursive(arr, target, low, high):
 
     middle = (low+high)/2
